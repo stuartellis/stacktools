d4262c3848d564760e98d324c0bc94685b0615f0
Stuart Ellis <stuart@stuartellis.name>
2023-01-21 19:39:48 +0000

    Move to src directory

--- src/make/tools/stacktools/stacktools-core.mk
+++ src/make/tools/stacktools/stacktools-core.mk
@@ -2,15 +2,14 @@
 #
 # Makefile targets and variables
 #
-# Requirements: A UNIX shell, jq, GNU Make 3 or above
-#
 
 ###### Versions ######
 
-ST_STACKTOOLS_VERSION		:= 0.4.15
+ST_STACKTOOLS_VERSION		:= 0.4.16
 ST_STACKTOOLS_REPO_URL		:= git@gitlab.com:stuartellis-org/stacktools.git
 ST_STACKTOOLS_REPO_BRANCH	:= main
 ST_STACKTOOLS_PKG			:= stacktools-DELETE-ME.tar
+ST_STACKTOOLS_SRC_DIR		:= src
 
 ST_STACKS_SPEC_VERSION		:= 0.4.0
 ST_STACKS_SPEC_URL			:= https://gitlab.com/stuartellis-org/stacktools/-/tree/main/docs/terraform-stacks-spec/$(ST_STACKS_SPEC_VERSION)/README.md
@@ -29,10 +28,13 @@ stacktools-info:
 
 .PHONY: stacktools-init
 stacktools-init:
-	@git archive --remote $(ST_STACKTOOLS_REPO_URL) --format tar --output $(ST_STACKTOOLS_PKG) $(ST_STACKTOOLS_REPO_BRANCH)
-	@tar -xzf $(ST_STACKTOOLS_PKG) docker/tools/stacktools make/tools/stacktools
-	@make stacks-new-tree ST_ENABLE_BACKEND=false
-	@rm $(ST_STACKTOOLS_PKG)
+	@mkdir -p .stacktools
+	@git archive --remote $(ST_STACKTOOLS_REPO_URL) --format tar --output .stacktools/$(ST_STACKTOOLS_PKG) $(ST_STACKTOOLS_REPO_BRANCH)
+	@tar -xzf $(ST_STACKTOOLS_PKG) $(ST_STACKTOOLS_SRC_DIR)
+	mv .stacktools/$(ST_STACKTOOLS_SRC_DIR)/terraform1 .
+	mv .stacktools/$(ST_STACKTOOLS_SRC_DIR)/docker/stacktools .
+	mv .stacktools/$(ST_STACKTOOLS_SRC_DIR)/make/stacktools .
+	@rm -r .stacktools
 	@make stacktools-info
 
 .PHONY: stacktools-update
